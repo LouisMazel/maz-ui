caf95c34486654d14d244855b6fba96269b646b9
Mazel <mazuel.loic@gmail.com>
2019-12-26 18:34:07 +0100

    (building) Add Gulpfile & tasks to compile SCSS

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const { src, series, dest, watch } = require('gulp')
+const sass = require('gulp-sass')
+const rename = require('gulp-rename')
+const prefix = require('gulp-autoprefixer')
+const plumber = require('gulp-plumber')
+const notify = require('gulp-notify')
+const sourcemaps = require('gulp-sourcemaps')
+
+const onError = (err) => {
+  notify.onError({
+    title:    'Gulp',
+    subtitle: 'Failure!',
+    message:  'Error: <%= error.message %>',
+    sound:    'Basso'
+  })(err)
+}
+
+const sassOptions = {
+  outputStyle: 'expanded'
+}
+
+// BUILD SUBTASKS
+// ---------------
+
+const compile = () => {
+  return src('src/index.scss')
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync(sassOptions).on('error', sass.logError))
+    .pipe(prefix())
+    .pipe(rename('maz-ui.css'))
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest('./dist/'))
+}
+
+const watcher = () => {
+  watch('src/**/*.scss', compile)
+}
+
+// BUILD TASKS
+// ------------
+
+exports.default = series(compile, watcher)
+exports.compileScss = compile
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    'postcss-import': {},
+    'postcss-url': {}
   }
 }
