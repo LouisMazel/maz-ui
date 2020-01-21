2f29692514afbb15e48099fd87c3fe417aa57580
Mazel <mazuel.loic@gmail.com>
2020-01-21 22:58:27 +0100

    (clean) Remove theme dir

--- src/theme/_subs/Vars.js
+++
@@ -1,6 +0,0 @@
-export const color = 'dodgerblue'
-export const darkColor = '#21222E'
-export const validColor = 'yellowgreen'
-export const borderRadius = 4
-export const lightColor = 'white'
-export const errorColor = 'orangered'
