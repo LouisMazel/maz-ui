d783f7af486e48275a18a5ade2f7697a13a79901
Mazel <mazuel.loic@gmail.com>
2020-05-08 15:08:53 +0200

    feat(CSS): gen css files to improve bundle size

--- build/gen-cssfile.js
+++ build/gen-cssfile.js
@@ -1,11 +1,11 @@
-var fs = require('fs')
-var path = require('path')
-var Components = require('./components.js')
-var themes = [
+const fs = require('fs')
+const path = require('path')
+let Components = require('./components.js')
+const themes = [
   'components'
 ]
 Components = Object.keys(Components)
-var basepath = path.resolve(__dirname, './../packages/scss')
+const basepath = path.resolve(__dirname, './../packages/scss')
 
 const fileExists = (filePath) => {
   try {
@@ -16,18 +16,17 @@ const fileExists = (filePath) => {
 }
 
 themes.forEach((theme) => {
-  var isSCSS = theme !== 'components'
+  const isSCSS = theme !== 'components'
   console.log('key')
-  var indexContent = isSCSS ? '@import "./base.scss";\n' : '@import "./base.css";\n'
+  let indexContent = isSCSS ? '@import "./base.scss";\n' : '@import "./base.css";\n'
   Components.forEach((key) => {
     console.log('key', key)
-    if (['icon', 'option', 'option-group'].indexOf(key) > -1) return
-    var fileName = key + (isSCSS ? '.scss' : '.css')
+    const fileName = key + (isSCSS ? '.scss' : '.css')
     indexContent += '@import "./' + fileName + '";\n'
-    var filePath = path.resolve(basepath, theme, fileName)
+    const filePath = path.resolve(basepath, theme, fileName)
     if (!fileExists(filePath)) {
-      fs.writeFileSync(filePath, '', 'utf8')
-      console.log(theme, ' 创建遗漏的 ', fileName, ' 文件')
+      // fs.writeFileSync(filePath, '', 'utf8')
+      console.log(theme, fileName)
     }
   })
   fs.writeFileSync(path.resolve(basepath, theme, isSCSS ? 'index.scss' : 'index.css'), indexContent)
