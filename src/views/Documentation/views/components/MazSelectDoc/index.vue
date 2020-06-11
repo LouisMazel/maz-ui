e8430f7a54e5feb10b2891df192cc57a5115dd13
Mazel <mazuel.loic@gmail.com>
2020-06-11 20:56:13 +0200

    [MazSelect] Add the multiple option

--- src/views/Documentation/views/components/MazSelectDoc/index.vue
+++ src/views/Documentation/views/components/MazSelectDoc/index.vue
@@ -9,7 +9,6 @@
         MazInput
       </router-link> and therefore inherits all these options
     </p>
-
     <ComponentContainer
       code="<template>
   <MazSelect
@@ -35,12 +34,17 @@
   }
 </script>"
     >
+      <p class="maz-mb-3">
+        <strong>v-model:</strong> {{ selectValueExample }}
+      </p>
       <MazSelect
         v-model="selectValueExample"
         placeholder="Custom label & placeholder text"
         :options="options"
+        clearable
       />
     </ComponentContainer>
+
     <ComponentContainer
       code="<template>
   <MazSelect
@@ -49,18 +53,15 @@
     search
     search-placeholder=&quot;Search in options&quot;
   >
-    <i
-      slot=&quot;icon-left&quot;
-      class=&quot;material-icons&quot;
-    >
-      add
-    </i>
     <template
       v-slot=&quot;{ option }&quot;
     >
       <div
         v-if=&quot;option.value&quot;
         class=&quot;maz-flex maz-flex-start&quot;
+        :class=&quot;{
+          'maz-text-white': option.isSelected
+        }&quot;
       >
         <div class=&quot;maz-label maz-label-secondary&quot;>
           {\{ option.label }\}
@@ -74,7 +75,7 @@
 </template>"
     >
       <h4>
-        Search - custom options list - custom left icon
+        Search - custom options list
       </h4>
       <p class="maz-mb-3 maz-text-muted">
         Select input with search textfield
@@ -85,18 +86,15 @@
         search
         search-placeholder="Search in options"
       >
-        <i
-          slot="icon-left"
-          class="material-icons"
-        >
-          add
-        </i>
         <template
           v-slot="{ option }"
         >
           <div
             v-if="option.value"
             class="maz-flex maz-flex-start"
+            :class="{
+              'maz-text-white': option.isSelected
+            }"
           >
             <div class="maz-label maz-label-secondary">
               {{ option.label }}
@@ -108,7 +106,53 @@
         </template>
       </MazSelect>
     </ComponentContainer>
-    <ComponentContainer>
+    <ComponentContainer
+      code="<template>
+  <MazSelect
+    v-model=&quot;multipleValues&quot;
+    :options=&quot;optionsMultiple&quot;
+    search
+    size=&quot;lg&quot;
+    multiple
+    search-placeholder=&quot;Search in options&quot;
+    color=&quot;info&quot;
+  >
+    <i
+      slot=&quot;icon-left&quot;
+      class=&quot;material-icons&quot;
+    >
+      add_circle_outline
+    </i>
+  </MazSelect>
+</template>"
+    >
+      <h4>
+        Multiple - custom left icon
+      </h4>
+      <p class="maz-mb-3 maz-text-muted">
+        You can choose multiple values
+      </p>
+      <p class="maz-mb-3">
+        <strong>v-model:</strong> {{ multipleValues }}
+      </p>
+      <MazSelect
+        v-model="multipleValues"
+        :options="optionsMultiple"
+        search
+        size="lg"
+        multiple
+        search-placeholder="Search in options"
+        color="info"
+      >
+        <i
+          slot="icon-left"
+          class="material-icons"
+        >
+          add_circle_outline
+        </i>
+      </MazSelect>
+    </ComponentContainer>
+    <!-- <ComponentContainer>
       <h4 class="maz-mb-3">
         Basic
       </h4>
@@ -159,7 +203,7 @@
         :options="options"
         success
       />
-    </ComponentContainer>
+    </ComponentContainer> -->
 
     <hr
       id="howToUseIt"
@@ -207,13 +251,22 @@ export default {
   name: 'MazInputDoc',
   data () {
     return {
-      selectValue: '',
-      selectValueExample: '',
+      selectValue: null,
+      selectValueExample: null,
       options: [
         { label: 'None', value: null },
         { label: 'First option', value: 'first' },
         { label: 'Second option', value: 'second' },
         { label: 'Third option', value: 'third' }
+      ],
+      multipleValues: ['first', 'fourth'],
+      optionsMultiple: [
+        { label: 'None', value: null },
+        { label: 'First', value: 'first' },
+        { label: 'Second', value: 'second' },
+        { label: 'Third', value: 'third' },
+        { label: 'Fourth', value: 'fourth' },
+        { label: 'Fifth', value: 'fifth' }
       ],
       inputColors: COLORS
     }
