2ef1af2581c31d9f2f653dda0ced1b0c7ab7687c
Mazel <mazuel.loic@gmail.com>
2020-06-28 19:35:04 +0200

    [Documentation] Fix meta & img links

--- documentation/config/link.js
+++ documentation/config/link.js
@@ -1,8 +1,8 @@
 export default [
-  { rel: 'apple-touch-icon', sizes: '180x180', href: 'icons/apple-touch-icon.png' },
-  { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'icons/favicon-32x32.png' },
-  { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'icons/favicon-16x16.png' },
-  { rel: 'manifest', href: 'icons/site.webmanifest' },
-  { rel: 'mask-icon', href: 'icons/safari-pinned-tab.svg', color: '#5bbad5' },
-  { rel: 'shortcut icon', href: 'icons/favicon.ico' }
+  { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' },
+  { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' },
+  { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' },
+  { rel: 'manifest', href: '/icons/site.webmanifest' },
+  { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#5bbad5' },
+  { rel: 'shortcut icon', href: '/icons/favicon.ico' }
 ]
