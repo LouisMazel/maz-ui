62bda31930fc05c465b8de90a7cf7bdaff61d139
Mazel <mazuel.loic@gmail.com>
2020-06-28 14:59:54 +0200

    [Documentation] Optional chaining config

---
+++ documentation/babel.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  presets: [
+    [
+      '@vue/cli-plugin-babel/preset',
+      {
+        useBuiltIns: 'entry'
+      }
+    ]
+  ],
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current'
+            }
+          }
+        ]
+      ]
+    }
+  },
+  plugins: ['@babel/plugin-proposal-optional-chaining']
+}
