3d132005b6440459ce17e0bc8b5bcdec8ccb537d
LouisMazel <mazuel.loic@gmail.com>
2022-05-19 00:13:04 +0200

    fix(docs): server components rendering

--- packages/docs/docs/.vuepress/client.ts
+++ packages/docs/docs/.vuepress/client.ts
@@ -6,38 +6,37 @@ const mazIconPath = process.env.NODE_ENV === 'production'
     ? '/maz-ui-3/icons'
     : '/icons'
 
-import { ToasterOptions, AosOptions } from 'maz-ui'
+import { ToasterOptions, installToaster, installWait, installAos, AosOptions } from 'maz-ui'
+import components from 'maz-ui/components'
 
 export default defineClientConfig({
   enhance: async ({ app, router }) => {
+    app.provide('mazIconPath', mazIconPath)
+
+    Object.entries(components).forEach(([componentName, component]) => {
+      app.component(componentName, component)
+    })
+
+    const toasterOptions: ToasterOptions = {
+      persistent: false,
+      position: 'bottom-right',
+      timeout: 10000,
+    }
+
+
+    app.use(installToaster, toasterOptions)
+    app.use(installWait)
+
+    const aosOptions: AosOptions = {
+      router,
+      delay: 500,
+      animation: {
+        duration: 400,
+        once: false
+      }
+    }
     // @ts-ignore
     if (!__VUEPRESS_SSR__) {
-      const components = (await import('maz-ui/components')).default
-      const { installToaster, installWait, installAos } = await import('maz-ui')
-
-      app.provide('mazIconPath', mazIconPath)
-
-      Object.entries(components).forEach(([componentName, component]) => {
-        app.component(componentName, component)
-      })
-
-      const toasterOptions: ToasterOptions = {
-        persistent: false,
-        position: 'bottom-right',
-        timeout: 10000,
-      }
-
-
-      app.use(installToaster, toasterOptions)
-      app.use(installWait)
-      const aosOptions: AosOptions = {
-        router,
-        delay: 500,
-        animation: {
-          duration: 400,
-          once: false
-        }
-      }
       app.use(installAos, aosOptions)
     }
   }
