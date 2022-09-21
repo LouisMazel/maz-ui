4129588a8cdcf74b957433a40a5ad74225840c6f
LouisMazel <mazuel.loic@gmail.com>
2022-09-21 14:14:28 +0200

    fix(testing): reinit files

--- packages/testing/src/App.vue
+++ packages/testing/src/App.vue
@@ -15,25 +15,19 @@
 
     <!-- Start Developing Area - Do not commit anything here -->
 
-    <MazPhoneNumberInput v-model="phone" />
-    <MazSelect v-model="phone" />
-
     <!-- End Developing Area -->
   </main>
 </template>
 
 <script lang="ts" setup>
   import { onMounted, ref } from 'vue'
-  import { MazBtn, MazIcon, MazPhoneNumberInput } from 'maz-ui/package/components'
+  import { MazBtn, MazIcon } from 'maz-ui/package/components'
   import { aosInstance } from 'maz-ui/package/plugins'
   import { useThemeHandler, ThemeHandlerOptions } from 'maz-ui/package/helpers'
-  import MazSelect from 'maz-ui/package/components/MazSelect.vue';
 
   const options: ThemeHandlerOptions = {
     storageThemeKey: 'mode',
   }
-
-  const phone = ref()
 
   const { autoSetTheme, toggleTheme, hasDarkTheme } = useThemeHandler(options)
 
