69a6acf8c760d3a5178e3a65bc6caed5b27d055f
LouisMazel <mazuel.loic@gmail.com>
2022-12-03 15:31:02 +0100

    refactor(testing): styleling config ignore v-bind function

--- packages/testing/stylelint.config.js
+++ packages/testing/stylelint.config.js
@@ -7,6 +7,8 @@ module.exports = {
     'stylelint-config-standard-scss',
     'stylelint-config-tailwindcss',
   ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configurationg
   rules: {
     'at-rule-no-unknown': [
       true,
@@ -23,5 +25,6 @@ module.exports = {
     'selector-class-pattern': undefined,
     'no-descending-specificity': undefined,
     'string-quotes': 'single',
+    'function-no-unknown': [true, { ignoreFunctions: ['v-bind'] }],
   },
 }
