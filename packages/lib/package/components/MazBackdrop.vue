f03e070aaecc6809461e83664585afcfde252a50
LouisMazel <mazuel.loic@gmail.com>
2022-12-06 00:01:14 +0100

    refactor(lib): maz-backdrop - improvements & include style

--- packages/lib/package/components/MazBackdrop.vue
+++ packages/lib/package/components/MazBackdrop.vue
@@ -1,18 +1,15 @@
 <template>
-  <Teleport to="body" :selector="selector">
+  <Teleport :to="teleportSelector">
     <Transition
       appear
-      :name="transition"
+      :name="transitionName"
       @after-enter="onBackdropAnimationEnter"
       @after-leave="onBackdropAnimationLeave"
     >
       <div
         v-if="present"
-        :class="[
-          'm-backdrop',
-          { '--backdrop-present': present },
-          backdropClass,
-        ]"
+        class="m-backdrop"
+        :class="[{ '--backdrop-present': present }, backdropClass]"
         tabindex="-1"
         role="dialog"
       >
@@ -24,12 +21,12 @@
         />
         <div
           class="m-backdrop-content"
-          :class="modalClass"
+          :class="backdropContentClass"
           v-bind="$attrs"
           role="document"
           tabindex="0"
         >
-          <slot :close="() => close()"></slot>
+          <slot :close="close"></slot>
         </div>
       </div>
     </Transition>
@@ -37,100 +34,209 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, ref, type PropType, watch } from 'vue'
-
-  const MODAL_OPENED_CLASS = '--backdrop-present'
-
-  const addClassToDocument = () =>
-    document.documentElement.classList.add(MODAL_OPENED_CLASS)
-  const removeClassFromDocument = () =>
-    document.documentElement.classList.remove(MODAL_OPENED_CLASS)
+  import { defineComponent } from 'vue'
 
   export default defineComponent({
     inheritAttrs: false,
-
-    props: {
-      modelValue: { type: Boolean, default: true },
-      selector: { type: String, default: '#modals-container' },
-      beforeClose: {
-        type: Function as PropType<() => boolean>,
-        default: undefined,
-      },
-      persistent: { type: Boolean, default: false },
-      backdropClass: { type: [Array, String, Object], default: () => [] },
-      transition: { type: String, default: 'backdrop-anim' },
-      escKey: { type: Boolean, default: true },
-      zOffset: { type: Number, default: 20 },
-      defaultZIndex: { type: Number, default: 1050 },
-      modalClass: { type: String, default: undefined },
-    },
-
-    emits: ['open', 'close', 'update:model-value'],
-
-    setup(props, { emit }) {
-      const present = ref(props.modelValue)
-
-      const $toggleBackdrop = (show: boolean) => (present.value = show)
-
-      const openModal = () => $toggleBackdrop(true)
-      const close = () => $toggleBackdrop(false)
-
-      const toggleModal = (show: boolean, ...rest: unknown[]) => {
-        /* eslint-disable unicorn/prefer-logical-operator-over-ternary */
-        const guard: (...args: unknown[]) => boolean = props.beforeClose
-          ? props.beforeClose
-          : () => true
-        /* eslint-enable unicorn/prefer-logical-operator-over-ternary */
-
-        if (show) {
-          openModal()
-        } else if (!props.beforeClose || guard(...rest) === false) {
-          close()
-        }
-      }
-
-      const onBackdropAnimationEnter = () => {
-        emit('open')
-      }
-
-      const onBackdropAnimationLeave = () => {
-        emit('update:model-value', false)
-        emit('close')
-      }
-
-      const onBackdropClicked = () => {
-        if (!props.persistent) toggleModal(false)
-      }
-
-      const onKeyPress = (event: KeyboardEvent) => {
-        if (props.escKey && event.key === 'Escape' && !props.persistent)
-          toggleModal(false)
-      }
-
-      watch(
-        () => props.modelValue,
-        (value) => {
-          present.value = value
-          if (value) {
-            addClassToDocument()
-            window.addEventListener('keyup', onKeyPress, false)
-          } else {
-            window.removeEventListener('keyup', onKeyPress)
-            removeClassFromDocument()
-          }
-        },
-        // { immediate: true },
-      )
-
-      return {
-        present,
-        openModal,
-        close,
-        onBackdropClicked,
-        onBackdropAnimationEnter,
-        onBackdropAnimationLeave,
-        toggleModal,
-      }
-    },
   })
 </script>
+
+<script lang="ts" setup>
+  import { ref, type PropType, watch, onMounted } from 'vue'
+
+  const MODAL_OPENED_CLASS = '--backdrop-present'
+
+  const addClassToDocument = () => {
+    document.documentElement.classList.add(MODAL_OPENED_CLASS)
+  }
+
+  const removeClassFromDocument = () => {
+    const backdropPresents = document.querySelector(
+      '.m-backdrop.--backdrop-present',
+    )
+
+    if (!backdropPresents) {
+      document.documentElement.classList.remove(MODAL_OPENED_CLASS)
+    }
+  }
+
+  const props = defineProps({
+    modelValue: { type: Boolean, default: false },
+    teleportSelector: { type: String, default: 'body' },
+    beforeClose: {
+      // eslint-disable-next-line @typescript-eslint/ban-types
+      type: Function as PropType<Function>,
+      default: undefined,
+    },
+    persistent: { type: Boolean, default: false },
+    noCloseOnEscKey: { type: Boolean, default: false },
+    transitionName: { type: String, default: 'backdrop-anim' },
+    backdropClass: { type: [Array, String, Object], default: undefined },
+    backdropContentClass: { type: [Array, String, Object], default: undefined },
+  })
+
+  const emits = defineEmits([
+    'open',
+    'before-close',
+    'close',
+    'update:model-value',
+  ])
+
+  const present = ref(props.modelValue)
+
+  const close = () => {
+    toggleModal(false)
+  }
+
+  const toggleModal = async (show: boolean) => {
+    if (!show) {
+      emits('before-close')
+      await props.beforeClose?.()
+    }
+
+    present.value = show
+  }
+
+  const onBackdropAnimationEnter = () => {
+    emits('open')
+  }
+
+  const onBackdropAnimationLeave = () => {
+    emits('update:model-value', false)
+    emits('close')
+  }
+
+  const onBackdropClicked = () => {
+    if (!props.persistent) {
+      close()
+    }
+  }
+
+  const onKeyPress = (event: KeyboardEvent) => {
+    if (!props.noCloseOnEscKey && event.key === 'Escape' && !props.persistent) {
+      close()
+    }
+  }
+
+  const addClassAndEventToDocument = () => {
+    addClassToDocument()
+    document.addEventListener('keyup', onKeyPress, false)
+  }
+
+  const removeClassAndEventToDocument = () => {
+    document.removeEventListener('keyup', onKeyPress)
+    removeClassFromDocument()
+  }
+
+  onMounted(() => {
+    if (props.modelValue) addClassAndEventToDocument()
+  })
+
+  watch(
+    () => props.modelValue,
+    (value) => {
+      present.value = value
+
+      if (value) {
+        addClassAndEventToDocument()
+      } else {
+        removeClassAndEventToDocument()
+      }
+    },
+  )
+</script>
+
+<style lang="postcss">
+  html.--backdrop-present {
+    overflow-y: hidden;
+    height: 100vh;
+  }
+</style>
+
+<style lang="postcss">
+  .m-backdrop.bottom-sheet-anim-enter-active,
+  .m-backdrop.bottom-sheet-anim-leave-active {
+    transition: opacity ease-in-out 250ms;
+
+    & .m-backdrop-content {
+      transition: transform ease-in-out 250ms;
+      transform: translateY(0);
+    }
+  }
+
+  .m-backdrop.bottom-sheet-anim-enter-from,
+  .m-backdrop.bottom-sheet-anim-leave-to {
+    opacity: 0;
+
+    & .m-backdrop-content {
+      transform: translateY(100%);
+    }
+  }
+
+  .m-backdrop.modal-anim-enter-active,
+  .m-backdrop.modal-anim-leave-active {
+    transition: opacity ease-in-out 250ms;
+
+    & .m-backdrop-content {
+      transition: transform ease-in-out 250ms;
+      transform: translateY(0);
+    }
+  }
+
+  .m-backdrop.modal-anim-enter-from,
+  .m-backdrop.modal-anim-leave-to {
+    opacity: 0;
+
+    & .m-backdrop-content {
+      transform: translateY(-25px);
+    }
+  }
+
+  .m-backdrop.backdrop-anim-enter-active,
+  .m-backdrop.backdrop-anim-leave-active {
+    transition: opacity ease-in-out 250ms;
+  }
+
+  .m-backdrop.backdrop-anim-enter-from,
+  .m-backdrop.backdrop-anim-leave-to {
+    opacity: 0;
+  }
+
+  .m-backdrop {
+    @apply maz-fixed maz-inset-0 maz-z-default-backdrop maz-flex maz-opacity-100 maz-flex-center;
+
+    backdrop-filter: blur(3px);
+
+    &-overlay {
+      touch-action: none;
+
+      @apply maz-absolute maz-inset-0 maz-bg-overlay;
+
+      &:not(.--disabled) {
+        @apply maz-cursor-pointer;
+      }
+    }
+
+    &-content {
+      @apply maz-relative maz-z-1 focus:maz-outline-none;
+    }
+
+    &.--bottom-sheet {
+      & .m-backdrop-content {
+        @apply maz-absolute maz-bottom-0 maz-left-0 maz-right-0;
+      }
+    }
+
+    &.--fullscreen {
+      @apply maz-items-start mob-l:maz-items-center;
+
+      &.--center-top {
+        @apply maz-items-start mob-l:maz-pt-28;
+      }
+
+      & .m-backdrop-content {
+        @apply maz-relative maz-w-full mob-l:maz-w-auto;
+      }
+    }
+  }
+</style>
