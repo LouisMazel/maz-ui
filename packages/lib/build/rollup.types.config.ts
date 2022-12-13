3dba08e7e2ec0953675f23aec220188e2d4168c7
LouisMazel <mazuel.loic@gmail.com>
2022-12-13 02:03:56 +0100

    refactor(lib): use vite

--- packages/lib/build/rollup.types.config.ts
+++ packages/lib/build/rollup.types.config.ts
@@ -1,12 +1,14 @@
-import { resolve } from 'node:path'
+import { resolve, dirname } from 'node:path'
 import dts from 'rollup-plugin-dts'
 
-const INPUT_TYPES = resolve(__dirname, './../types/index.d.ts')
+const __dirname = dirname(import.meta.url.replace('file://', ''))
+
+const INPUT_TYPES_FILE = resolve(__dirname, './../types/index.d.ts')
 const OUTPUT_PATH = resolve(__dirname, './../dist/maz-ui.d.ts')
 
 export default [
   {
-    input: [INPUT_TYPES],
+    input: INPUT_TYPES_FILE,
     output: [{ file: OUTPUT_PATH, format: 'es' }],
     plugins: [dts()],
   },
