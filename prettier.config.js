daa06e6f81d73c3fe6a73c61ff3cc0b58e66fe3a
LouisMazel <mazuel.loic@gmail.com>
2022-12-23 01:57:49 +0100

    chore(root): add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  printWidth: 100,
+  endOfLine: 'auto',
+  vueIndentScriptAndStyle: true,
+}
