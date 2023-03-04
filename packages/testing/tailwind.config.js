10a2036bacc07a8748d6f4d7ad38ab5ff2567a97
LouisMazel <mazuel.loic@gmail.com>
2023-03-04 04:36:10 +0100

    fix(testing): correct paths to lib modules

--- packages/testing/tailwind.config.js
+++ packages/testing/tailwind.config.js
@@ -1,6 +1,6 @@
 // Tailwind CSS configuration (https://tailwindcss.com/docs/configuration)
 
-const tailwindConfig = require('maz-ui/package/tailwindcss/tailwind.config.js')
+const tailwindConfig = require('./../lib/package/tailwindcss/tailwind.config.js')
 
 module.exports = {
   mode: 'jit',
