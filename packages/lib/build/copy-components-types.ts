d9e2f9ec215164e84178a6afd03032941811906c
LouisMazel <mazuel.loic@gmail.com>
2023-03-06 15:01:18 +0100

    chore: npm to pnpm

--- packages/lib/build/copy-components-types.ts
+++ packages/lib/build/copy-components-types.ts
@@ -1,4 +1,4 @@
-import { copyFileSync, readdirSync, renameSync } from 'node:fs'
+import { copyFileSync, existsSync, mkdirSync, readdirSync, renameSync, statSync } from 'node:fs'
 import { resolve, join } from 'node:path'
 import { logger } from './utils/logger'
 import { replaceInFile } from 'replace-in-file'
@@ -7,17 +7,22 @@ const INPUT_COMPONENT_DIR = resolve(__dirname, './../generated-types/components'
 const OUTPUT_TYPES_FILES = resolve(__dirname, './../dist/components')
 const COMPONENTS_TYPE_PATH = resolve(__dirname, './../dist/components/index.d.ts')
 
-function copyRecursive() {
+function copyRecursive(inputPath: string, outputPath: string) {
   try {
-    for (const childItemName of readdirSync(INPUT_COMPONENT_DIR)) {
-      if (childItemName.endsWith('.d.ts')) {
-        copyFileSync(
-          join(INPUT_COMPONENT_DIR, childItemName),
-          join(OUTPUT_TYPES_FILES, childItemName),
-        )
-      } else {
-        continue
+    const exists = existsSync(inputPath)
+    const stats = statSync(inputPath)
+    const isDirectory = exists && stats.isDirectory()
+
+    if (isDirectory) {
+      const destDirExists = existsSync(outputPath)
+      if (!destDirExists) {
+        mkdirSync(outputPath)
       }
+      for (const childItemName of readdirSync(inputPath)) {
+        copyRecursive(join(inputPath, childItemName), join(outputPath, childItemName))
+      }
+    } else {
+      copyFileSync(inputPath, outputPath)
     }
   } catch (error) {
     throw new Error(`[copy-components-types](copyRecursive) ${error}`)
@@ -46,14 +51,14 @@ const replaceTypesExtensions = () => {
 
 export function copyAndTransformComponentsTypesFiles() {
   try {
-    copyRecursive()
+    copyRecursive(INPUT_COMPONENT_DIR, OUTPUT_TYPES_FILES)
     renameAllFiles()
     replaceTypesExtensions()
 
-    logger.success('[copy-components-types] ✅')
+    logger.success('[copy-components-types] declaration types files copied ✅')
   } catch (error) {
     logger.error(
-      '[copy-components-types](launch) 🔴 Error occurred while copying component type files',
+      '[copy-components-types] 🔴 Error occurred while copying component type files',
       error,
     )
   }
