64e16c86e8ff1c565538c6ef32c48364d930495c
LouisMazel <mazuel.loic@gmail.com>
2023-03-07 14:49:00 +0100

    feat(lib): maz-dialog - add max-height & scrollable options

--- packages/docs/docs/components/maz-dialog.md
+++ packages/docs/docs/components/maz-dialog.md
@@ -3,6 +3,8 @@ title: MazDialog
 description: MazDialog is a standalone dialog component to show important informations to the user or propose specific action. Many options are available. You can hide the header or the footer, full-size layout, differents states etc.
 ---
 
+<!-- markdownlint-disable MD033 -->
+
 # {{ $frontmatter.title }}
 
 {{ $frontmatter.description }}
@@ -21,17 +23,12 @@ This component usex `<Teleport to="body">` with [MazBackdrop](./maz-backdrop.md)
   <p>
     Your content
   </p>
-  <template #footer>
-    <MazBtn @click="isOpen = false">
+  <template #footer="{ close }">
+    <MazBtn @click="close">
       Confirm
     </MazBtn>
   </template>
 </MazDialog>
-
-<script setup>
-  import { ref } from 'vue'
-  const isOpen = ref(false)
-</script>
 
 ```vue
 <template>
@@ -41,8 +38,8 @@ This component usex `<Teleport to="body">` with [MazBackdrop](./maz-backdrop.md)
     <p>
       Your content
     </p>
-    <template #footer>
-      <MazBtn @click="isOpen = false">
+    <template #footer="{ close }">
+      <MazBtn @click="close">
         Confirm
       </MazBtn>
     </template>
@@ -57,4 +54,114 @@ This component usex `<Teleport to="body">` with [MazBackdrop](./maz-backdrop.md)
 </script>
 ```
 
+## Scrollable
+
+For long content, you can enable scrolling in content part (Header and footer slot remain visible at top and bottom)
+
+::: warning
+With this option, an overflow is applied: So, some problems with absolute content may appear - such as `<MazSelect />` opened list can not be visible outside the dialog content part, you should scroll again to show the full content - **Check example below**
+:::
+
+<MazBtn @click="scollableOpened = true">Open Scrollable Dialog</MazBtn>
+
+<MazDialog v-model="scollableOpened" title="Dialog Title" max-height="400px" scrollable>
+  <template #title>
+    Scrollable Dialog Title
+  </template>
+
+  <template #default>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <MazSelect
+      :model-value="1"
+      :options="[
+        { value: 1, label: 1 },
+        { value: 2, label: 2 },
+        { value: 3, label: 3 },
+        { value: 4, label: 4 },
+        { value: 5, label: 5 },
+        { value: 6, label: 6 },
+      ]"
+    />
+  </template>
+  <template #footer>
+    <MazBtn @click="scollableOpened = false">
+      Confirm
+    </MazBtn>
+  </template>
+</MazDialog>
+
+```html
+<MazDialog v-model="scollableOpened" title="Dialog Title" max-height="400px" scrollable>
+  <template #title>
+    Scrollable Dialog Title
+  </template>
+
+  <template #default>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <p class="maz-py-4">
+      Scroll
+    </p>
+    <MazSelect
+      :model-value="1"
+      :options="[
+        { value: 1, label: 1 },
+        { value: 2, label: 2 },
+        { value: 3, label: 3 },
+        { value: 4, label: 4 },
+        { value: 5, label: 5 },
+        { value: 6, label: 6 },
+      ]"
+    />
+  </template>
+  <template #footer>
+    <MazBtn @click="scollableOpened = false">
+      Confirm
+    </MazBtn>
+  </template>
+</MazDialog>
+```
+
 <!--@include: ./../.vitepress/generated-docs/maz-dialog.doc.md-->
+
+<script setup>
+  import { ref } from 'vue'
+  const isOpen = ref(false)
+  const scollableOpened = ref(false)
+</script>
--- packages/lib/package/components/MazDialog.vue
+++ packages/lib/package/components/MazDialog.vue
@@ -1,32 +1,54 @@
 <template>
   <MazBackdrop
-    v-bind="$attrs"
+    v-bind="backdropAttrs"
     transition-name="modal-anim"
     @close="$emit('close', $event)"
     @open="$emit('open', $event)"
     @update:model-value="$emit('update:model-value', $event)"
   >
     <template #default="{ close }">
-      <div class="m-dialog-layout" :style="[{ width, maxWidth }]" :class="[wrapperClasses]">
-        <div class="m-dialog-layout-header" :class="{ '--has-title': $slots['title'] || title }">
-          <h2 v-if="$slots['title'] || title" class="maz-my-0 maz-text-xl maz-font-semibold">
-            <slot name="title">{{ title }}</slot>
-          </h2>
+      <div
+        class="m-dialog"
+        :style="[{ width, maxWidth, maxHeight }]"
+        :class="{ '--scrollable': scrollable }"
+        v-bind="wrapperAttrs"
+      >
+        <!--
+          @slot Header slot
+            @binding {Function} close close function
+        -->
+        <slot name="header" :close="close">
+          <div class="m-dialog-header" :class="{ '--has-title': $slots['title'] || title }">
+            <h2 v-if="$slots['title'] || title" class="maz-my-0 maz-text-xl maz-font-semibold">
+              <!--
+                @slot Title slot in the header
+              -->
+              <slot name="title">{{ title }}</slot>
+            </h2>
 
-          <MazBtn
-            v-if="!noClose"
-            size="sm"
-            class="m-dialog-layout-closebtn"
-            color="transparent"
-            @click="close"
-          >
-            <MazIcon :src="XIcon" class="maz-h-5 maz-w-5" />
-          </MazBtn>
-        </div>
-        <div class="m-dialog-layout-content">
+            <MazBtn
+              v-if="!noClose"
+              size="sm"
+              class="m-dialog-closebtn"
+              color="transparent"
+              @click="close"
+            >
+              <MazIcon :src="XIcon" class="maz-h-5 maz-w-5" />
+            </MazBtn>
+          </div>
+        </slot>
+        <div class="m-dialog-content">
+          <!--
+            @slot Default content
+              @binding {Function} close close function
+          -->
           <slot :close="close" />
         </div>
-        <div v-if="$slots['footer']" class="m-dialog-layout-footer">
+        <div v-if="$slots['footer']" class="m-dialog-footer">
+          <!--
+            @slot Footer slot
+              @binding {Function} close close function
+          -->
           <slot name="footer" :close="close"></slot>
         </div>
       </div>
@@ -39,21 +61,48 @@
   import MazBackdrop from './MazBackdrop.vue'
   import MazIcon from './MazIcon.vue'
   import XIcon from '@package/icons/x.svg'
+  import { computed, useAttrs } from 'vue'
 
   defineProps({
+    /** Modal's title in  header */
     title: { type: String, default: undefined },
+    /** Remove the close button in header */
     noClose: { type: Boolean, default: false },
+    /** Modal's width */
     width: { type: String, default: '500px' },
+    /** Modal's max-width */
     maxWidth: { type: String, default: '95vw' },
-    wrapperClasses: { type: [String, Object, Array], default: undefined },
+    /** Modal's max-height */
+    maxHeight: { type: String, default: '95vh' },
+    /**  Modal's content becomes scrollable - warning: a overflow is applied */
+    scrollable: { type: Boolean, default: false },
   })
 
-  defineEmits(['update:model-value', 'close', 'open'])
+  defineEmits([
+    /** Model binding */
+    'update:model-value',
+    /** Emitted when dialog is closed (after animation) */
+    'close',
+    /** Emitted when dialog is opened (after animation) */
+    'open',
+  ])
+
+  const attrs = useAttrs()
+
+  const backdropAttrs = computed(() => ({
+    ...attrs,
+    class: undefined,
+    style: undefined,
+  }))
+  const wrapperAttrs = computed(() => ({
+    class: attrs.class,
+    style: attrs.style,
+  }))
 </script>
 
 <style lang="postcss" scoped>
-  .m-dialog-layout {
-    @apply maz-rounded maz-bg-color maz-text-normal;
+  .m-dialog {
+    @apply maz-flex maz-flex-col maz-rounded maz-bg-color maz-text-normal;
 
     &-header {
       @apply maz-flex maz-items-center maz-justify-end maz-pt-2 maz-pl-6 maz-pr-2;
@@ -68,10 +117,16 @@
     }
 
     &-content {
-      @apply maz-p-6;
+      @apply maz-flex-1 maz-p-6;
 
       &-icon {
         flex: 0 0 auto;
+      }
+    }
+
+    &.--scrollable {
+      .m-dialog-content {
+        @apply maz-overflow-auto;
       }
     }
 
