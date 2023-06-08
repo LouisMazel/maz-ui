3a43630e7b7b2ee818c8a14f449054183a569d7f
LouisMazel <me@loicmazuel.com>
2023-06-08 20:09:43 +0200

    chore(lib): use @vitest/coverage-v8 instead of c8

--- packages/lib/vitest.config.ts
+++ packages/lib/vitest.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
     },
     globals: true,
     coverage: {
+      provider: 'v8',
       all: true,
       excludeNodeModules: true,
       reporter: ['clover', 'html'],
