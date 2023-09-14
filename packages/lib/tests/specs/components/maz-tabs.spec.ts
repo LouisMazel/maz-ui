9f54c1139d7b0287d9382044e7c0dc6a7e2583da
LouisMazel <me@loicmazuel.com>
2023-09-14 17:51:44 -0400

    test(lib): add test for maz-tabs

---
+++ packages/lib/tests/specs/components/maz-tabs.spec.ts
@@ -0,0 +1,26 @@
+import { mount } from '@vue/test-utils'
+import MazTabs from '@components/MazTabs.vue'
+
+describe('MazTabs.vue', () => {
+  test('Should match with the snapshot', () => {
+    const wrapper = mount(MazTabs)
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('should display the content', () => {
+    const wrapper = mount(MazTabs, {
+      slots: {
+        default: '<div>Contenu du slot</div>',
+      },
+    })
+
+    expect(wrapper.text()).toContain('Contenu du slot')
+  })
+
+  test('initialize component with good value', () => {
+    const wrapper = mount(MazTabs)
+
+    expect(wrapper.vm.$props.modelValue).toBe(1)
+  })
+})
