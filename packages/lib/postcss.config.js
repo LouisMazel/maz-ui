d3b1e949e2783c50face4de057d6da217edae304
LouisMazel <me@loicmazuel.com>
2023-09-18 12:04:14 -0300

    fix(lib): use postcss-nested to compile css

--- packages/lib/postcss.config.js
+++ packages/lib/postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
   syntax: 'postcss-scss',
   plugins: {
     'postcss-url': {},
+    'postcss-nested': {},
     'tailwindcss/nesting': {},
     'postcss-import': {},
     autoprefixer: {},
