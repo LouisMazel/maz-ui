fbc716272211ed065afe05134abd625fe81013e4
LouisMazel <me@loicmazuel.com>
2023-09-21 16:15:28 -0400

    refactor(playground): update use of nuxt module

--- packages/playground/src/pages/index.vue
+++ packages/playground/src/pages/index.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1 data-maz-aos="fade-right">Home</h1>
 
     <!-- Start Developping Area - You should not commit anything here to keep this place clean for all others -->
 
     <!-- End Developping Area -->
+
+    <MazFullscreenLoader v-show="wait.isLoading('APP_LOADING')" color="secondary">
+      Loading...
+    </MazFullscreenLoader>
   </div>
 </template>
 
 <script lang="ts" setup>
+  import { sleep } from 'maz-ui'
+
   const toast = useToast()
+  const wait = useWait()
+
+  useIdleTimeout({
+    // eslint-disable-next-line no-console
+    callback: (payload) => console.log('idle', payload),
+    options: {
+      timeout: 1000,
+    },
+  })
+
+  useUserVisibility({
+    // eslint-disable-next-line no-console
+    callback: (payload) => console.log('userVisibility', payload),
+    options: {
+      timeout: 1000,
+    },
+  })
+
+  wait.start('APP_LOADING')
+
+  onMounted(async () => {
+    await sleep(1000)
+
+    wait.stop('APP_LOADING')
+  })
 
   toast.info('Votre mot de passe a été mis à jour', {
     position: 'top-left',
