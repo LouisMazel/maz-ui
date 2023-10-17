302db88d89fa20149e655ac361a7d4cb78e8c6b6
LouisMazel <me@loicmazuel.com>
2023-10-17 17:35:40 -0400

    test(lib): add test units for MazBottomSheet component

---
+++ packages/lib/tests/specs/components/maz-bottom-sheet.spec.ts
@@ -0,0 +1,52 @@
+import { mount } from '@vue/test-utils'
+import MazBottomSheet from '@components/MazBottomSheet.vue'
+
+describe('MazBottomSheet', () => {
+  it('renders the component', () => {
+    const wrapper = mount(MazBottomSheet, {
+      props: {
+        noClose: false,
+        noPadding: false,
+      },
+    })
+
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('emits open event when the backdrop is opened', async () => {
+    const wrapper = mount(MazBottomSheet, {
+      props: {
+        noClose: false,
+        noPadding: false,
+      },
+    })
+
+    await wrapper.findComponent({ name: 'MazBackdrop' }).vm.$emit('open')
+
+    expect(wrapper.emitted('open')).toBeTruthy()
+  })
+
+  it('emits close event when the backdrop is closed', async () => {
+    const wrapper = mount(MazBottomSheet, {
+      props: {
+        noClose: false,
+        noPadding: false,
+      },
+    })
+
+    await wrapper.findComponent({ name: 'MazBackdrop' }).vm.$emit('close')
+
+    expect(wrapper.emitted('close')).toBeTruthy()
+  })
+
+  it('does not render the close button when noClose prop is true', () => {
+    const wrapper = mount(MazBottomSheet, {
+      props: {
+        noClose: true,
+        noPadding: false,
+      },
+    })
+
+    expect(wrapper.findComponent({ name: 'MazBtn' }).exists()).toBe(false)
+  })
+})
