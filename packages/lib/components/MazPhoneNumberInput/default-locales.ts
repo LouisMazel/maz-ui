54a51bbb45654ddb126907a8d3f8d4d8192c9b1a
LouisMazel <me@loicmazuel.com>
2023-10-22 17:55:22 -0400

    refactor(lib): MazPhoneNumberInput - add option to choose locale of countries list

---
+++ packages/lib/components/MazPhoneNumberInput/default-locales.ts
@@ -0,0 +1,11 @@
+export const defaultLocales = {
+  countrySelector: {
+    placeholder: 'Country code',
+    error: 'Choose country',
+    searchPlaceholder: 'Search the country',
+  },
+  phoneInput: {
+    placeholder: 'Phone number',
+    example: 'Example:',
+  },
+}
