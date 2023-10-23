66ab3b368b5d2cc0a85137185d6fe76c0c418ba4
LouisMazel <me@loicmazuel.com>
2023-10-23 14:25:52 -0400

    chore(lib): remove old unused components

--- packages/lib/components_tmp/MazPagination/index.js
+++
@@ -1,7 +0,0 @@
-import MazPagination from './_main.vue'
-
-MazPagination.install = (Vue) => {
-  Vue.component(MazPagination.name, MazPagination)
-}
-
-export default MazPagination
--- packages/lib/components_tmp/MazStepper/index.js
+++
@@ -1,7 +0,0 @@
-import MazStepper from './_main.vue'
-
-MazStepper.install = (Vue) => {
-  Vue.component(MazStepper.name, MazStepper)
-}
-
-export default MazStepper
