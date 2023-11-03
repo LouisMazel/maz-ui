240fee3c715b4676c3a29c3cc511f7c779aff9c2
LouisMazel <me@loicmazuel.com>
2023-11-03 16:47:02 +0100

    refactor(lib): MazCheckbox - style and animations improvements + size option support

--- packages/lib/components/MazCheckbox.vue
+++ packages/lib/components/MazCheckbox.vue
@@ -1,24 +1,35 @@
 <template>
-  <div class="m-checkbox" :class="[`m-checkbox--${color}`]">
+  <label
+    :for="instanceId"
+    class="m-checkbox"
+    :class="{ '--disabled': disabled }"
+    tabindex="1"
+    @keydown="keyboardHandler"
+  >
     <input
       :id="instanceId"
       :checked="modelValue"
       v-bind="$attrs"
+      :disabled="disabled"
       :name="name"
       type="checkbox"
-      @change="$emit('update:model-value', ($event?.target as HTMLInputElement)?.checked)"
+      @change="emitValue(($event?.target as HTMLInputElement)?.checked)"
     />
-    <label :for="instanceId">
-      <slot></slot>
-    </label>
-  </div>
+    <span>
+      <Transition name="maz-scale">
+        <CheckIcon v-show="modelValue" class="check-icon" :class="checkIconSize" />
+      </Transition>
+    </span>
+    <slot></slot>
+  </label>
 </template>
 
 <script lang="ts" setup>
   import { useInstanceUniqId } from '../modules/composables/use-instance-uniq-id'
-  import { type PropType, getCurrentInstance } from 'vue'
-  import type { Color } from './types'
-  export type { Color }
+  import { type PropType, getCurrentInstance, computed } from 'vue'
+  import type { Color, Size } from './types'
+  export type { Color, Size }
+  import CheckIcon from './../icons/check.svg'
 
   const instance = getCurrentInstance()
 
@@ -30,275 +41,132 @@
       default: 'primary',
     },
     name: { type: String, default: 'm-checkbox' },
+    size: { type: String as PropType<Size>, default: 'md' },
+    disabled: { type: Boolean, default: false },
   })
-  defineEmits(['update:model-value'])
+  const emits = defineEmits([
+    /* emitted when value change */
+    'update:model-value',
+    /* emited when value change */
+    'change',
+  ])
 
   const instanceId = useInstanceUniqId({
     componentName: 'MazCheckbox',
     instance,
     providedId: props.id,
   })
+
+  const checkboxSize = computed(() => {
+    switch (props.size) {
+      case 'xl': {
+        return '2.25rem'
+      }
+      case 'lg': {
+        return '2rem'
+      }
+      default: {
+        return '1.625rem'
+      }
+      case 'sm': {
+        return '1.425rem'
+      }
+      case 'xs': {
+        return '1.325rem'
+      }
+      case 'mini': {
+        return '1.2rem'
+      }
+    }
+  })
+
+  const checkIconSize = computed(() => {
+    switch (props.size) {
+      case 'xl': {
+        return 'maz-text-2xl'
+      }
+      case 'lg': {
+        return 'maz-text-xl'
+      }
+      default: {
+        return 'maz-text-lg'
+      }
+      case 'sm': {
+        return 'maz-text-base'
+      }
+      case 'xs': {
+        return 'maz-text-sm'
+      }
+      case 'mini': {
+        return 'maz-text-xs'
+      }
+    }
+  })
+
+  const checkIconColor = computed(() => `var(--maz-color-${props.color})`)
+  const checkboxBorderColor = computed(() => `var(--maz-color-${props.color})`)
+  const checkboxBoxShadow = computed(() =>
+    ['black', 'transparent'].includes(props.color)
+      ? `var(--maz-color-muted)`
+      : `var(--maz-color-${props.color}-alpha)`,
+  )
+
+  function keyboardHandler(event: KeyboardEvent) {
+    if (['Enter', 'Space'].includes(event.code)) {
+      event.preventDefault()
+      emitValue(!props.modelValue)
+    }
+  }
+
+  function emitValue(value: boolean) {
+    emits('update:model-value', value)
+    emits('change', value)
+  }
 </script>
 
 <style lang="postcss" scoped>
   .m-checkbox {
-    transition: all 300ms ease-in-out;
-    min-height: 1.75rem;
+    @apply maz-relative maz-inline-flex maz-items-center maz-gap-2 maz-outline-none;
 
-    @apply maz-cursor-pointer;
+    .check-icon {
+      color: v-bind('checkIconColor');
 
-    [type='checkbox']:not(:checked),
-    [type='checkbox']:checked {
-      @apply maz-absolute;
-
-      left: -9999px;
-    }
-
-    [type='checkbox']:not(:checked) + label,
-    [type='checkbox']:checked + label {
-      @apply maz-relative maz-m-0 maz-flex maz-cursor-pointer maz-select-none maz-items-center maz-pl-10 maz-leading-7;
-
-      transition: all 300ms ease-in-out;
-    }
-
-    [type='checkbox'] + label::before {
-      @apply maz-absolute maz-left-0 maz-rounded maz-border maz-border-solid maz-border-transparent maz-bg-transparent;
-
-      content: '';
-      width: 1.625rem;
-      height: 1.625rem;
-      background: transparent;
-      transition: all 300ms ease-in-out;
-    }
-
-    [type='checkbox']:hover + label::before,
-    [type='checkbox']:focus + label::before {
-      @apply maz-absolute maz-border maz-border-solid maz-border-primary maz-bg-transparent;
-
-      content: '';
-    }
-
-    [type='checkbox']:not(:checked) + label::before {
-      @apply maz-border-color-light;
-    }
-
-    [type='checkbox']:not(:checked) + label::after,
-    [type='checkbox']:checked + label::after {
-      content: '';
-
-      @apply maz-absolute;
-
-      left: 0.438rem;
-      width: 0.75rem;
-      height: 0.75rem;
-      border-radius: 0.125rem;
-    }
-
-    [type='checkbox']:not(:checked) + label::after {
-      @apply maz-scale-0 maz-opacity-0;
-    }
-
-    [type='checkbox']:checked + label::after {
-      @apply maz-scale-100 maz-opacity-100;
-    }
-
-    &--primary {
-      [type='checkbox']:checked + label::before {
-        @apply maz-border-primary;
-      }
-
-      [type='checkbox']:not(:checked) + label::after,
-      [type='checkbox']:checked + label::after {
-        @apply maz-bg-primary maz-text-primary;
-      }
-
-      [type='checkbox']:focus,
-      [type='checkbox']:hover {
-        + label::before {
-          box-shadow: 0 0 0 0.125rem var(--maz-color-primary-alpha);
-
-          @apply maz-border-primary-700;
-        }
-
-        + label::after {
-          @apply maz-bg-primary-700;
-        }
+      :deep(path) {
+        stroke-width: 2.5;
       }
     }
 
-    &--secondary {
-      [type='checkbox']:checked + label::before {
-        @apply maz-border-secondary;
-      }
+    span {
+      @apply maz-relative maz-flex maz-rounded maz-border maz-border-color-light maz-transition-all maz-duration-300 maz-ease-in-out maz-flex-center dark:maz-border-color-lighter;
 
-      [type='checkbox']:not(:checked) + label::after,
-      [type='checkbox']:checked + label::after {
-        @apply maz-bg-secondary maz-text-secondary;
-      }
+      width: v-bind('checkboxSize');
+      height: v-bind('checkboxSize');
+    }
 
-      [type='checkbox']:focus,
-      [type='checkbox']:hover {
-        + label::before {
-          box-shadow: 0 0 0 0.125rem var(--maz-color-secondary-alpha);
+    &.--disabled {
+      @apply maz-cursor-not-allowed;
+    }
 
-          @apply maz-border-secondary-700;
-        }
+    &:not(.--disabled) {
+      @apply maz-cursor-pointer;
 
-        + label::after {
-          @apply maz-bg-secondary-700;
-        }
+      &:hover span,
+      &:focus span {
+        @apply maz-transition-all maz-duration-300 maz-ease-in-out;
+
+        box-shadow: 0 0 0 0.125rem v-bind('checkboxBoxShadow');
       }
     }
 
-    &--info {
-      [type='checkbox']:checked + label::before {
-        @apply maz-border-info;
+    input {
+      @apply maz-hidden;
+
+      &:disabled ~ span {
+        @apply maz-bg-color-light;
       }
 
-      [type='checkbox']:not(:checked) + label::after,
-      [type='checkbox']:checked + label::after {
-        @apply maz-bg-info maz-text-info;
-      }
-
-      [type='checkbox']:focus,
-      [type='checkbox']:hover {
-        + label::before {
-          box-shadow: 0 0 0 0.125rem var(--maz-color-info-alpha);
-
-          @apply maz-border-info-700;
-        }
-
-        + label::after {
-          @apply maz-bg-info-700;
-        }
-      }
-    }
-
-    &--danger {
-      [type='checkbox']:checked + label::before {
-        @apply maz-border-danger;
-      }
-
-      [type='checkbox']:not(:checked) + label::after,
-      [type='checkbox']:checked + label::after {
-        @apply maz-bg-danger maz-text-danger;
-      }
-
-      [type='checkbox']:focus,
-      [type='checkbox']:hover {
-        + label::before {
-          box-shadow: 0 0 0 0.125rem var(--maz-color-danger-alpha);
-
-          @apply maz-border-danger-700;
-        }
-
-        + label::after {
-          @apply maz-bg-danger-700;
-        }
-      }
-    }
-
-    &--warning {
-      [type='checkbox']:checked + label::before {
-        @apply maz-border-warning;
-      }
-
-      [type='checkbox']:not(:checked) + label::after,
-      [type='checkbox']:checked + label::after {
-        @apply maz-bg-warning maz-text-warning;
-      }
-
-      [type='checkbox']:focus,
-      [type='checkbox']:hover {
-        + label::before {
-          box-shadow: 0 0 0 0.125rem var(--maz-color-warning-alpha);
-
-          @apply maz-border-warning-700;
-        }
-
-        + label::after {
-          @apply maz-bg-warning-700;
-        }
-      }
-    }
-
-    &--success {
-      [type='checkbox']:checked + label::before {
-        @apply maz-border-success;
-      }
-
-      [type='checkbox']:not(:checked) + label::after,
-      [type='checkbox']:checked + label::after {
-        @apply maz-bg-success maz-text-success;
-      }
-
-      [type='checkbox']:focus,
-      [type='checkbox']:hover {
-        + label::before {
-          box-shadow: 0 0 0 0.125rem var(--maz-color-success-alpha);
-
-          @apply maz-border-success-700;
-        }
-
-        + label::after {
-          @apply maz-bg-success-700;
-        }
-      }
-    }
-
-    &--white {
-      [type='checkbox']:checked + label::before {
-        @apply maz-border-white;
-      }
-
-      [type='checkbox']:not(:checked) + label::after,
-      [type='checkbox']:checked + label::after {
-        @apply maz-bg-white maz-text-white;
-      }
-
-      [type='checkbox']:focus,
-      [type='checkbox']:hover {
-        + label::before {
-          box-shadow: 0 0 0 0.125rem hsl(216deg 12% 84% / 60%);
-
-          @apply maz-border-gray-300;
-        }
-
-        + label::after {
-          @apply maz-bg-gray-300;
-        }
-      }
-    }
-
-    &--black {
-      [type='checkbox']:checked + label::before {
-        @apply maz-border-black;
-      }
-
-      [type='checkbox']:not(:checked) + label::after,
-      [type='checkbox']:checked + label::after {
-        @apply maz-bg-black maz-text-black;
-      }
-
-      [type='checkbox']:focus,
-      [type='checkbox']:hover {
-        + label::before {
-          box-shadow: 0 0 0 0.125rem hsl(0deg 0% 0% / 60%);
-
-          @apply maz-border-gray-700;
-        }
-
-        + label::after {
-          @apply maz-bg-gray-700;
-        }
-      }
-    }
-  }
-
-  html.dark {
-    .m-checkbox {
-      [type='checkbox']:not(:checked) + label::before {
-        @apply maz-border-color-lighter;
+      &:checked ~ span {
+        border-color: v-bind('checkboxBorderColor');
       }
     }
   }
