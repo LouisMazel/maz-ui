52ce7aca4f9c86aa92c6fcd7b23732fa265a6ca5
LouisMazel <me@loicmazuel.com>
2023-11-03 16:58:50 +0100

    test(lib): MazCheckbox - add test units

--- packages/lib/tests/specs/components/maz-checkbox.spec.ts
+++ packages/lib/tests/specs/components/maz-checkbox.spec.ts
@@ -1,20 +1,47 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import MazCheckbox from '@components/MazCheckbox.vue'
 
 describe('components/MazCheckbox.vue', () => {
   expect(MazCheckbox).toBeTruthy()
 
-  const wrapper = shallowMount(MazCheckbox, {
-    props: {
-      modelValue: false,
-    },
+  test('Should match with the snapshot', () => {
+    const wrapper = mount(MazCheckbox, {
+      props: {
+        modelValue: false,
+      },
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+
+    wrapper.unmount()
   })
 
-  test('Should match with the snapshot', () => {
-    expect(wrapper.html()).toMatchSnapshot()
+  test('renders correctly', () => {
+    const wrapper = mount(MazCheckbox, {
+      props: {
+        modelValue: true, // You can provide props here
+        // Add other props as needed
+      },
+    })
+
+    // Add your assertions based on your component's rendering and behavior
+    // For example, you can assert the existence of specific elements or classes:
+    expect(wrapper.find('label').exists()).toBe(true)
+    expect(wrapper.find('input').attributes('type')).toBe('checkbox')
+    // Add more assertions as needed.
+
+    // Don't forget to clean up the wrapper
+    wrapper.unmount()
   })
 
   test('Should have an uniq id', () => {
-    expect(wrapper.vm.instanceId).toBe('MazCheckbox-1')
+    const wrapper = mount(MazCheckbox, {
+      props: {
+        modelValue: false,
+      },
+    })
+
+    expect(wrapper.vm.instanceId).toBe('MazCheckbox-9')
+
+    wrapper.unmount()
   })
 })
