34c0c0393095bd57580290104da0f52d33f87ffe
LouisMazel <me@loicmazuel.com>
2023-11-21 19:09:34 +0100

    fix(cli): linter error

--- packages/cli/docgen.config.js
+++ packages/cli/docgen.config.js
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const { resolve, join } = require('node:path')
+const path = require('node:path')
 const { defineConfig } = require('vue-docgen-cli')
 
 module.exports = defineConfig({
-  componentsRoot: resolve(__dirname, './../lib/components'),
+  componentsRoot: path.resolve(__dirname, './../lib/components'),
   components: './[A-Z]*.vue', // the glob to define what files should be documented as components (relative to componentRoot)
-  outDir: resolve(__dirname, './../docs/docs/.vitepress/generated-docs'),
+  outDir: path.resolve(__dirname, './../docs/docs/.vitepress/generated-docs'),
   getDestFile: (filename, config) => {
     const filenameKebab = filename.replaceAll(/([\da-z])([A-Z])/g, '$1-$2').toLowerCase()
 
-    return join(config.outDir, filenameKebab).replace(/\.vue$/, '.doc.md')
+    return path.join(config.outDir, filenameKebab).replace(/\.vue$/, '.doc.md')
   },
   templates: {
     component: require('./config/docgen/template-component'),
