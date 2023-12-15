a1a4e949f606dab332e8d9d1984ecf9891af2fc5
LouisMazel <me@loicmazuel.com>
2023-12-15 16:08:47 +0100

    feat(maz-ui): useSwipe - new composable to handling mobile swipe

---
+++ packages/lib/modules/composables/use-swipe.ts
@@ -0,0 +1,35 @@
+import { ref } from 'vue'
+import { Swipe, type SwipeOptions } from '../helpers/swipe-handler'
+
+export function useSwipe(options: Omit<SwipeOptions, 'onValuesChanged'>) {
+  const swiper = new Swipe({
+    ...options,
+    element: options.element,
+    onValuesChanged(values) {
+      xDiff.value = values.xDiff
+      yDiff.value = values.yDiff
+      xStart.value = values.xStart
+      xEnd.value = values.xEnd
+      yStart.value = values.yStart
+      yEnd.value = values.yEnd
+    },
+  })
+
+  const xDiff = ref<number>()
+  const yDiff = ref<number>()
+  const xStart = ref<number>()
+  const xEnd = ref<number>()
+  const yStart = ref<number>()
+  const yEnd = ref<number>()
+
+  return {
+    xDiff,
+    yDiff,
+    xStart,
+    xEnd,
+    yStart,
+    yEnd,
+    start: swiper.start,
+    stop: swiper.stop,
+  }
+}
