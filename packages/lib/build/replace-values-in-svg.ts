641ae182e047869842c7f11de7e15a381f8508e5
LouisMazel <me@loicmazuel.com>
2023-12-19 02:11:08 +0100

    refactor(maz-ui): icons - update icons pack

---
+++ packages/lib/build/replace-values-in-svg.ts
@@ -0,0 +1,41 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import { logger } from './utils/logger'
+import { fileURLToPath } from 'node:url'
+
+const _dirname = fileURLToPath(new URL('.', import.meta.url))
+
+const SVGFolder = path.resolve(_dirname, '../icons')
+
+fs.readdir(SVGFolder, (err, fichiers) => {
+  if (err) {
+    console.error('Error while reading file :', err)
+    return
+  }
+
+  for (const fichier of fichiers) {
+    const filePath = path.join(SVGFolder, fichier)
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        logger.error(`Error while reading file ${fichier} :`, err)
+        return
+      }
+
+      const modifyContent = data
+        .replaceAll('width="24"', 'width="1em"')
+        .replaceAll('height="24"', 'height="1em"')
+        // Remplacez les codes couleur hexadécimaux par "currentColor"
+        .replaceAll(/stroke="#[\dA-Fa-f]{6}"/g, 'stroke="currentColor"')
+        .replaceAll(/fill="#[\dA-Fa-f]{6}"/g, 'fill="currentColor"')
+
+      fs.writeFile(filePath, modifyContent, 'utf8', (err) => {
+        if (err) {
+          logger.error(`Error while writing file ${fichier} :`, err)
+        } else {
+          logger.log(`Attributs updated for ${fichier}`)
+        }
+      })
+    })
+  }
+})
