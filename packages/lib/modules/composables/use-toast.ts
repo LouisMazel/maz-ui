f2dccccd1f312cbe1c4d8686f3de9baa269d27eb
LouisMazel <me@loicmazuel.com>
2024-04-05 18:08:15 +0200

    refactor(maz-ui): composables - function syntax instead of arrow function

--- packages/lib/modules/composables/use-toast.ts
+++ packages/lib/modules/composables/use-toast.ts
@@ -1,6 +1,6 @@
 import { injectStrict } from '../helpers/inject-strict'
 import type { ToasterHandler } from '../plugins/toaster'
 
-export const useToast = () => {
+export function useToast() {
   return injectStrict<ToasterHandler>('toast')
 }
