abb4fb16f867eb3aeb49370105c4eba027752c51
LouisMazel <me@loicmazuel.com>
2024-10-26 13:38:36 +0200

    chore(release): bump version to 3.47.2-beta.1

--- packages/lib/modules/composables/useFormValidator/useFormValidator.ts
+++ packages/lib/modules/composables/useFormValidator/useFormValidator.ts
@@ -10,7 +10,7 @@ import type {
   UseFormValidatorParams,
 } from './types'
 
-import { computed, nextTick, provide, ref, toValue, watch } from 'vue'
+import { computed, nextTick, provide, ref, watch } from 'vue'
 import { CONFIG } from './config'
 import {
   getErrorMessages,
@@ -40,7 +40,7 @@ export function useFormValidator<
 
   const internalDefaultValues = ref(defaultValues) as Ref<Partial<Model>>
   const payload = ref({ ...internalDefaultValues.value, ...model?.value }) as Ref<Model>
-  const internalSchema = toValue(schema) as Ref<FormSchema<Model>>
+  const internalSchema = ref(schema) as Ref<FormSchema<Model>>
   const fieldsStates = ref(
     getFieldsStates<Model>({
       schema: internalSchema.value,
