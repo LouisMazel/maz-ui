2438fdfb7862831690669ba52c2013cf77a434c4
LouisMazel <me@loicmazuel.com>
2024-11-24 16:59:39 +0100

    fix(maz-ui): typescript issue with generate tailwind config

--- packages/lib/tailwindcss/utils/colors.ts
+++ packages/lib/tailwindcss/utils/colors.ts
@@ -1,13 +1,15 @@
 import type { ThemeConfig } from 'tailwindcss/types/config'
 import { baseColors, variatingColors, variations } from '../variables/colors'
 
+type VariatingColorKeys = keyof typeof variatingColors
+
 export function getColors() {
-  const colors: Record<keyof typeof variatingColors & keyof typeof baseColors, string> = {
+  const colors: ThemeConfig['colors'] = {
     ...baseColors,
   }
 
-  for (const color of Object.keys(variatingColors)) {
-    colors[color] = variations.reduce(
+  for (const color of Object.keys(variatingColors) as VariatingColorKeys[]) {
+    const colorVariations = variations.reduce(
       (colorVariations, variation) => {
         colorVariations[variation]
           = variation === 'DEFAULT'
@@ -15,9 +17,11 @@ export function getColors() {
             : `var(--maz-color-${color}-${variation})`
         return colorVariations
       },
-      {} as Record<string, string>,
+      {} as Record<typeof variations[number], string>,
     )
+
+    colors[color] = colorVariations
   }
 
-  return colors satisfies ThemeConfig['colors']
+  return colors
 }
