6187cc2b0a19f639e31631a4b4180a3681a4b97a
LouisMazel <me@loicmazuel.com>
2024-12-08 03:09:55 +0100

    refactor(maz-ui): MazAnimateCounter - make it SSR friendly

--- packages/lib/tests/specs/components/MazAnimatedCounter.spec.ts
+++ packages/lib/tests/specs/components/MazAnimatedCounter.spec.ts
@@ -2,48 +2,27 @@ import MazAnimatedCounter from '@components/MazAnimatedCounter.vue'
 import { shallowMount } from '@vue/test-utils'
 
 describe('mazAnimatedCounter', () => {
-  it('renders initial count with prefix and suffix', async () => {
+  it('renders initial count with prefix and suffix', () => {
     const wrapper = shallowMount(MazAnimatedCounter, {
       props: { count: 10, prefix: '$' },
     })
 
     expect(wrapper.find('.maz-sr-only').text()).toBe('$10')
-    expect(wrapper.text()).toBe('$10$0')
-
-    await new Promise(resolve => setTimeout(resolve, 100))
-    expect(wrapper.text()).toBe('$10$0')
   })
 
-  it('updates count and triggers animation', async () => {
+  it('updates count and triggers animation', () => {
     const wrapper = shallowMount(MazAnimatedCounter, {
       props: { count: 10, suffix: '%' },
     })
 
     expect(wrapper.find('.maz-sr-only').text()).toBe('10%')
-    expect(wrapper.text()).toBe('10%0%')
-
-    await new Promise(resolve => setTimeout(resolve, 100))
-    expect(wrapper.text()).toBe('10%0%')
-
-    await wrapper.setProps({ count: 20, suffix: '%' })
-    expect(wrapper.find('.maz-sr-only').text()).toBe('20%')
-    expect(wrapper.text()).toBe('20%10%')
-
-    // await new Promise(resolve => setTimeout(resolve, 100))
-    // expect(wrapper.text()).toBe('20%-52100%')
   })
 
-  it('respects delay prop', async () => {
+  it('respects delay prop', () => {
     const wrapper = shallowMount(MazAnimatedCounter, {
       props: { count: 10, delay: 200 },
     })
 
     expect(wrapper.text()).toBe('100')
-
-    await new Promise(resolve => setTimeout(resolve, 100))
-    expect(wrapper.text()).toBe('100')
-
-    // await new Promise(resolve => setTimeout(resolve, 200))
-    // expect(wrapper.html()).toBe('10')
   })
 })
