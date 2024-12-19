0d199b890172a022e15975714e0ffc07c7e22790
LouisMazel <me@loicmazuel.com>
2024-12-19 17:13:04 -0500

    chore(maz-ui): build all lib

---
+++ packages/lib/build/BuildNuxtModule.ts
@@ -0,0 +1,32 @@
+import type { Plugin } from 'vite'
+
+import { execPromise } from './utils/exec-promise'
+import { logger } from './utils/logger'
+
+export function BuildNuxtModule(): Plugin {
+  return {
+    name: 'vite-build-nuxt-module',
+    async buildEnd() {
+      try {
+        await execPromise('pnpm -F nuxt-module build')
+        // Nuxt Module: rename all module.* to index.*
+        // const fileList = await readdir(resolve(__dirname, '../dist/nuxt'), {
+        //   withFileTypes: true,
+        // })
+
+        // const fileListToRename = fileList.filter(
+        //   dirent => dirent.isFile() && dirent.name.startsWith('module'),
+        // )
+
+        // for await (const { path: filePath, name } of fileListToRename) {
+        //   await rename(resolve(filePath, name), resolve(filePath, `index${extname(name)}`))
+        // }
+
+        logger.success('[BuildNuxtModule] ✅ nuxt module built')
+      }
+      catch (error) {
+        logger.error('[BuildNuxtModule] 🔴 error while building nuxt module', error)
+      }
+    },
+  }
+}
---
+++ packages/lib/build/CompileStyles.ts
@@ -0,0 +1,27 @@
+import type { Plugin } from 'vite'
+
+import { compileScss } from './compile-scss'
+import { execPromise } from './utils/exec-promise'
+import { logger } from './utils/logger'
+
+export function CompileStyles(): Plugin {
+  return {
+    name: 'vite-compile-styles',
+    async buildEnd() {
+      try {
+        await execPromise(
+          'tailwindcss -i src/tailwindcss/tailwind.css -o dist/css/main.css --config tailwind.config.ts --postcss --minify',
+        )
+
+        logger.success('[CompileStyles] ✅ tailwind css compiled')
+
+        await compileScss()
+
+        logger.success('[CompileStyles] ✅ scss compiled')
+      }
+      catch (error) {
+        logger.error('[CompileStyles] 🔴 error while compiling styles', error)
+      }
+    },
+  }
+}
