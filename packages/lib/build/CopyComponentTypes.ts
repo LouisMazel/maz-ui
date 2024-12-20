bebfcfbd86da1b740041245b3884f9ddf19de3e6
LouisMazel <me@loicmazuel.com>
2024-12-19 20:06:43 -0500

    chore(release): bump version to 4.0.0-alpha.5

---
+++ packages/lib/build/CopyComponentTypes.ts
@@ -0,0 +1,24 @@
+import type { Plugin } from 'vite'
+
+import { copyAndTransformComponentsTypesFiles } from './copy-components-types'
+import { execPromise } from './utils/exec-promise'
+import { logger } from './utils/logger'
+
+export function CopyComponentTypes(): Plugin {
+  return {
+    name: 'vite-copy-component-types',
+    async writeBundle() {
+      try {
+        await execPromise('vue-tsc --declaration --emitDeclarationOnly -p tsconfig.types.json')
+
+        copyAndTransformComponentsTypesFiles()
+        logger.success('[CopyComponentTypes] ✅ component types copied')
+      }
+      catch (error) {
+        logger.error('[CopyComponentTypes] 🔴 error while copying component types', error)
+
+        throw error
+      }
+    },
+  }
+}
