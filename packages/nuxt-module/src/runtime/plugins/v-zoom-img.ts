ecb0b612b19c7c0b21da6c76d028c5aaacbbe2b3
LouisMazel <me@loicmazuel.com>
2025-07-10 22:55:49 +0200

    refactor(@maz-ui/nuxt): improve typescript support of directives

--- packages/nuxt-module/src/runtime/plugins/v-zoom-img.ts
+++ packages/nuxt-module/src/runtime/plugins/v-zoom-img.ts
@@ -1,6 +1,12 @@
-import { vZoomImgInstall } from 'maz-ui/directives/vZoomImg'
+import { type VZoomImgDirective, vZoomImgInstall } from 'maz-ui/directives/vZoomImg'
 import { defineNuxtPlugin } from 'nuxt/app'
 
 export default defineNuxtPlugin(({ vueApp }) => {
   vueApp.use(vZoomImgInstall)
 })
+
+declare module 'vue' {
+  interface GlobalDirectives {
+    vZoomImg: VZoomImgDirective
+  }
+}
