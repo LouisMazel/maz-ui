0529f70a9cc1575c6cbe42f576b5134d2eca1f5d
LouisMazel <me@loicmazuel.com>
2025-07-10 10:10:50 +0200

    test(@maz-ui/utils): add and correct test units

---
+++ packages/utils/src/helpers/__tests__/deboune-callback.spec.ts
@@ -0,0 +1,33 @@
+import { debounceCallback } from '../debounceCallback'
+
+describe('debounceCallback.ts', () => {
+  it('delaying Callback Execution', () => {
+    vi.useFakeTimers()
+
+    const fakeCallback1 = vi.fn()
+
+    debounceCallback(fakeCallback1, 1000)
+
+    expect(fakeCallback1).not.toHaveBeenCalled()
+
+    vi.advanceTimersByTime(1000)
+
+    expect(fakeCallback1).toHaveBeenCalledTimes(1)
+  })
+
+  it('cancel Previous Callbacks', () => {
+    vi.useFakeTimers()
+
+    const fakeCallback2 = vi.fn()
+
+    debounceCallback(fakeCallback2, 1000)
+
+    vi.advanceTimersByTime(750)
+
+    debounceCallback(fakeCallback2, 1000)
+
+    vi.advanceTimersByTime(750)
+
+    expect(fakeCallback2).not.toHaveBeenCalled()
+  })
+})
