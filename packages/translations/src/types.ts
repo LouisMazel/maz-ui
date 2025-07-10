b1a2c08f73416e0d278cbe149fd9cc857e577f24
LouisMazel <me@loicmazuel.com>
2025-07-10 21:56:54 +0200

    docs(@maz-ui/translations): add documentation into type definitions

--- packages/translations/src/types.ts
+++ packages/translations/src/types.ts
@@ -5,15 +5,39 @@ import type defaultMessages from './locales/en'
 export type TranslationKey = NonNullable<DeepKeyOf<MazTranslationsSchema>>
 
 export interface MazTranslationsOptions {
+  /**
+   * The locale to use
+   * @default 'en'
+   */
   locale?: string
+  /**
+   * The fallback locale to use
+   * @default 'en'
+   */
   fallbackLocale?: string
+  /**
+   * Whether to preload the fallback locale
+   * @default true
+   */
   preloadFallback?: boolean
+  /**
+   * Modify existing or add new languages
+   */
   messages?: Record<string, MazTranslationsSchema | (() => Promise<MazTranslationsSchema>)>
 }
 
 export interface MazTranslationsInstance {
+  /**
+   * The current locale
+   */
   locale: Ref<string>
+  /**
+   * The translation function
+   */
   t: (key: TranslationKey, variables?: Record<string, any>) => string
+  /**
+   * The function to set the locale dynamically
+   */
   setLocale: (locale: string) => Promise<void>
 }
 
