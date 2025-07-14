059ec47df748c7f38cd16579dd46b69c3b9d66c0
LouisMazel <me@loicmazuel.com>
2025-07-14 16:28:06 +0200

    chore: generate changelog with changelogen

---
+++ eslint.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from '@maz-ui/eslint-config'
+
+export default defineConfig(
+  {
+    formatters: true,
+    typescript: true,
+    sonarjs: true,
+  },
+  {
+    ignores: ['./packages/**/*', './apps/**/*', './tools/**/*'],
+  },
+)
