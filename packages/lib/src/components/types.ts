0ed3c0563a1bf66cc7d3195b5008e3c698656021
LouisMazel <me@loicmazuel.com>
2025-07-16 03:01:03 +0200

    chore(maz-ui): map colors to replace background by surface

--- packages/lib/src/components/types.ts
+++ packages/lib/src/components/types.ts
@@ -3,3 +3,7 @@ import type { colors } from './constantes'
 export type MazColor = (typeof colors)[number]
 
 export type MazSize = 'mini' | 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+
+export function getColor(color: MazColor | 'background') {
+  return color === 'background' ? 'surface' : color
+}
