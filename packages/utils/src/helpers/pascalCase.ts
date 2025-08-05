41823d4987a40be9157372f60ad3faf9ecea18da
LouisMazel <me@loicmazuel.com>
2025-08-06 01:37:09 +0200

    refactor(@maz-ui/utils): improve pascalCase method and add test units

--- packages/utils/src/helpers/pascalCase.ts
+++ packages/utils/src/helpers/pascalCase.ts
@@ -2,5 +2,29 @@ import { camelCase } from './camelCase.js'
 import { capitalize } from './capitalize.js'
 
 export function pascalCase(str: string) {
+  if (str === str.toUpperCase()) {
+    if (str.includes('-')) {
+      return str.toLowerCase().split('-').map(capitalize).join('')
+    }
+    if (str.includes('_')) {
+      return str.toLowerCase().split('_').map(capitalize).join('')
+    }
+    if (str.includes(' ')) {
+      return str.toLowerCase().split(' ').map(capitalize).join('')
+    }
+  }
+
+  if (str.includes('-')) {
+    return str.toLowerCase().split('-').map(capitalize).join('')
+  }
+
+  if (str.includes('_')) {
+    return str.toLowerCase().split('_').map(capitalize).join('')
+  }
+
+  if (str.includes(' ')) {
+    return str.toLowerCase().split(' ').map(capitalize).join('')
+  }
+
   return capitalize(camelCase(str))
 }
