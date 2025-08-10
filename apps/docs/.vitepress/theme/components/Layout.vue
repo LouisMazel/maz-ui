1714340440a12a3f10af623dcbedd6d3a7eebedb
LouisMazel <me@loicmazuel.com>
2025-08-10 17:47:04 +0200

    chore(docs): fix top banner style

--- apps/docs/.vitepress/theme/components/Layout.vue
+++ apps/docs/.vitepress/theme/components/Layout.vue
@@ -10,7 +10,7 @@ import DefaultTheme from 'vitepress/theme'
           <span class="maz-text-white">
             You are currently browsing the v4 documentation -
           </span>
-          <MazLink href="https://v3.maz-ui.com" color="contrast" underline class="dark maz-w-full mob-l:maz-w-auto">
+          <MazLink href="https://v3.maz-ui.com" color="contrast" underline class="dark">
             View v3 documentation
           </MazLink>
         </div>
@@ -21,6 +21,6 @@ import DefaultTheme from 'vitepress/theme'
 
 <style lang="postcss" scoped>
 .banner {
-  @apply maz-bg-primary-600/90 maz-fixed maz-inset-x-0 maz-top-0 maz-flex maz-flex-wrap maz-items-center maz-justify-center maz-gap-2 maz-p-2 maz-z-default-backdrop maz-text-white maz-text-center maz-h-12 maz-backdrop-blur-lg;
+  @apply maz-bg-primary-600/90 maz-fixed maz-inset-x-0 maz-top-0 maz-flex maz-flex-wrap maz-flex-center maz-justify-center maz-gap-2 maz-p-2 maz-z-default-backdrop maz-text-white maz-text-center maz-backdrop-blur-lg maz-text-sm;
 }
 </style>
