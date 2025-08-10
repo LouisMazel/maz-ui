153bcf27be263ba10c3b9a36f17e279f830c9837
LouisMazel <me@loicmazuel.com>
2025-08-10 15:17:16 +0200

    chore(maz-ui): vite.config - build:watch command build in development mode

--- packages/lib/vite.config.ts
+++ packages/lib/vite.config.ts
@@ -51,69 +51,72 @@ const moduleEntries = Object.fromEntries(
     .map(getEntries),
 )
 
-export default defineConfig({
-  plugins: [
-    Vue(),
-    SvgLoader(),
-    libInjectCss(),
-    dts({
-      tsconfigPath: resolver('./tsconfig.json'),
-      entryRoot: resolver('src'),
-      outDir: [resolver('dist/types'), resolver('dist')],
-    }),
-    ViteCompileStyles(),
-  ],
-  esbuild: {
-    drop: ['debugger'],
-    pure: ['console.log', 'console.debug'],
-    legalComments: 'none',
-    target: 'es2022',
-    minifyIdentifiers: false, // to fix the issue "SyntaxError: Identifier 'h' has already been declared"
-    minifySyntax: true,
-    minifyWhitespace: true,
-    treeShaking: true,
-  },
-  build: {
-    cssCodeSplit: true,
-    emptyOutDir: true,
-    sourcemap: false,
-    cssMinify: 'lightningcss',
-    minify: 'esbuild',
-    target: 'es2022',
-    lib: {
-      entry: {
-        ...moduleEntries,
-        'components/index': resolver('src/components/index.ts'),
-        'composables/index': resolver('src/composables/index.ts'),
-        'plugins/index': resolver('src/plugins/index.ts'),
-        'directives/index': resolver('src/directives/index.ts'),
-        'resolvers/index': resolver('src/resolvers/index.ts'),
-        'tailwindcss/index': resolver('src/tailwindcss/index.ts'),
-        'index': resolver('src/index.ts'),
-      },
-      formats: ['es'],
-      fileName: (_, name) => `${name}.js`,
-      cssFileName: '[name].[hash].css',
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+  return {
+    plugins: [
+      Vue(),
+      SvgLoader(),
+      libInjectCss(),
+      dts({
+        tsconfigPath: resolver('./tsconfig.json'),
+        entryRoot: resolver('src'),
+        outDir: [resolver('dist/types'), resolver('dist')],
+      }),
+      ViteCompileStyles(),
+    ],
+    esbuild: {
+      drop: ['debugger'],
+      pure: isDev ? [] : ['console.log', 'console.debug'],
+      legalComments: 'none',
+      target: 'es2022',
+      minifyIdentifiers: false, // to fix the issue "SyntaxError: Identifier 'h' has already been declared"
+      minifySyntax: true,
+      minifyWhitespace: true,
+      treeShaking: true,
     },
-    rollupOptions: {
-      external,
-      treeshake: {
-        moduleSideEffects: false,
-        preset: 'smallest',
-        propertyReadSideEffects: false,
-        unknownGlobalSideEffects: false,
+    build: {
+      cssCodeSplit: true,
+      emptyOutDir: true,
+      sourcemap: false,
+      cssMinify: 'lightningcss',
+      minify: 'esbuild',
+      target: 'es2022',
+      lib: {
+        entry: {
+          ...moduleEntries,
+          'components/index': resolver('src/components/index.ts'),
+          'composables/index': resolver('src/composables/index.ts'),
+          'plugins/index': resolver('src/plugins/index.ts'),
+          'directives/index': resolver('src/directives/index.ts'),
+          'resolvers/index': resolver('src/resolvers/index.ts'),
+          'tailwindcss/index': resolver('src/tailwindcss/index.ts'),
+          'index': resolver('src/index.ts'),
+        },
+        formats: ['es'],
+        fileName: (_, name) => `${name}.js`,
+        cssFileName: '[name].[hash].css',
       },
-      output: {
-        format: 'es',
-        compact: true,
-        chunkFileNames: 'chunks/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash][extname]',
-        exports: 'named',
-        minifyInternalExports: true,
-        preserveModules: false,
-        interop: 'auto',
-        generatedCode: 'es2015',
+      rollupOptions: {
+        external,
+        treeshake: {
+          moduleSideEffects: false,
+          preset: 'smallest',
+          propertyReadSideEffects: false,
+          unknownGlobalSideEffects: false,
+        },
+        output: {
+          format: 'es',
+          compact: true,
+          chunkFileNames: 'chunks/[name].[hash].js',
+          assetFileNames: 'assets/[name].[hash][extname]',
+          exports: 'named',
+          minifyInternalExports: true,
+          preserveModules: false,
+          interop: 'auto',
+          generatedCode: 'es2015',
+        },
       },
     },
-  },
+  }
 })
