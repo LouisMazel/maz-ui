a20a248ba3d4a2297410e9f908ff1e79d4b63649
LouisMazel <me@loicmazuel.com>
2025-08-13 19:37:48 +0200

    chore(maz-ui): getColor mapper supports muted color

--- packages/lib/src/components/types.ts
+++ packages/lib/src/components/types.ts
@@ -4,6 +4,6 @@ export type MazColor = (typeof colors)[number]
 
 export type MazSize = 'mini' | 'xs' | 'sm' | 'md' | 'lg' | 'xl'
 
-export function getColor(color: MazColor | 'background') {
+export function getColor(color: MazColor | 'background' | 'muted') {
   return color === 'background' ? 'surface' : color
 }
