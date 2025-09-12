3278a91a243fbec137c9e381540df7b1baf3682e
LouisMazel <me@loicmazuel.com>
2025-09-13 00:43:18 +0200

    chore: update apps with updated theme plugin

--- apps/nuxt-app/src/components/ThemeSwitcher.vue
+++ apps/nuxt-app/src/components/ThemeSwitcher.vue
@@ -1,69 +1,43 @@
 <script lang="ts" setup>
-const { colorMode, updateTheme } = useTheme()
+const { colorMode, updateTheme, presetName, isDark } = useTheme()
 </script>
 
 <template>
   <MazDropdown
     id="dropdown"
     color="transparent"
+    transition="scale-fade"
     prefer-position="bottom-end"
-    trigger="hover"
   >
-    <span class="maz-capitalize">
-      {{ colorMode }}
+    <span class="maz-cap-f">
+      {{ colorMode }} ({{ isDark ? 'dark' : 'light' }})
     </span>
 
     <template #dropdown>
-      <div class="maz-grid maz-grid-cols-2">
-        <MazBtn
-          color="transparent"
-          class="maz-text-nowrap"
-          @click="colorMode = 'auto'"
-        >
-          Auto
-        </MazBtn>
-        <MazBtn
-          color="transparent"
-          class="maz-text-nowrap"
-          @click="colorMode = 'dark'"
-        >
-          Dark
-        </MazBtn>
-        <MazBtn
-          color="transparent"
-          class="maz-text-nowrap"
-          @click="colorMode = 'light'"
-        >
-          Light
-        </MazBtn>
-        <MazBtn
-          color="transparent"
-          class="maz-text-nowrap"
-          @click="updateTheme('maz-ui')"
-        >
-          Maz-UI
-        </MazBtn>
-        <MazBtn
-          color="transparent"
-          class="maz-text-nowrap"
-          @click="updateTheme('ocean')"
-        >
-          Ocean
-        </MazBtn>
-        <MazBtn
-          color="transparent"
-          class="maz-text-nowrap"
-          @click="updateTheme('obsidian')"
-        >
-          Obsidian
-        </MazBtn>
-        <MazBtn
-          color="transparent"
-          class="maz-text-nowrap"
-          @click="updateTheme('pristine')"
-        >
-          Pristine
-        </MazBtn>
+      <div class="maz-flex maz-max-w-60 maz-flex-col maz-gap-4">
+        <MazRadioButtons
+          v-model="colorMode"
+          :options="[
+            { label: 'Auto', value: 'auto' },
+            { label: 'Dark', value: 'dark' },
+            { label: 'Light', value: 'light' },
+          ]"
+        />
+
+        <p>
+          Theme
+        </p>
+
+        <MazRadioButtons
+          :model-value="presetName"
+          :options="[
+            { label: 'Maz-UI', value: 'maz-ui' },
+            { label: 'Ocean', value: 'ocean' },
+            { label: 'Obsidian', value: 'obsidian' },
+            { label: 'Pristine', value: 'pristine' },
+          ]"
+          @update:model-value="updateTheme($event)"
+        />
       </div>
     </template>
   </MazDropdown>
