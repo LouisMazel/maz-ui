e9614e873f3a7c765fa85f88594370632e2fff52
LouisMazel <me@loicmazuel.com>
2025-10-22 11:17:18 +0200

    refactor(@maz-ui/node): improve logger by using consola

--- packages/node/src/execPromise.ts
+++ packages/node/src/execPromise.ts
@@ -2,7 +2,22 @@ import type { Logger } from './logger.js'
 import { exec } from 'node:child_process'
 import { logger as defaultLogger } from './logger.js'
 
-export async function execPromise(command: string, { logger, packageName, noSuccess, noStdout, noStderr }: { logger?: Logger, packageName?: string, noSuccess?: boolean, noStdout?: boolean, noStderr?: boolean } = {}): Promise<{ stdout: string, stderr: string }> {
+export async function execPromise(
+  command: string,
+  {
+    logger,
+    packageName,
+    noSuccess,
+    noStdout,
+    noStderr,
+  }: {
+    logger?: Logger
+    packageName?: string
+    noSuccess?: boolean
+    noStdout?: boolean
+    noStderr?: boolean
+  } = {},
+): Promise<{ stdout: string, stderr: string }> {
   const internalLogger = logger ?? defaultLogger
   const packageNameStr = packageName ? `[${packageName}]` : ''
 
