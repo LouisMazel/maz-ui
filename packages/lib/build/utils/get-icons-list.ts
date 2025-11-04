a90df936b0c033614a216222952f7171ddfe4528
LouisMazel <me@loicmazuel.com>
2025-11-04 12:16:04 +0100

    chore(maz-ui): disable no-console rule in get-icon-list

--- packages/lib/build/utils/get-icons-list.ts
+++ packages/lib/build/utils/get-icons-list.ts
@@ -5,5 +5,6 @@ import { fileURLToPath } from 'node:url'
 const _dirname = fileURLToPath(new URL('.', import.meta.url))
 
 fs.readdir(resolve(_dirname, './../../icons'), (_, files) => {
+  // eslint-disable-next-line no-console
   console.log('files', files)
 })
