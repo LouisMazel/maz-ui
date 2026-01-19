5722932ff783c1d6afd22e01a50388e734ae7ee9
LouisMazel <me@loicmazuel.com>
2026-01-19 23:37:39 +0100

    refactor(docs): remove mention of v3

--- apps/docs/.vitepress/theme/components/Layout.vue
+++ apps/docs/.vitepress/theme/components/Layout.vue
@@ -5,7 +5,7 @@ import DefaultTheme from 'vitepress/theme'
 <template>
   <Suspense>
     <DefaultTheme.Layout>
-      <template #layout-top>
+      <!-- <template #layout-top>
         <div class="banner">
           <span class="maz-text-white">
             You are currently browsing the v4 documentation -
@@ -14,13 +14,13 @@ import DefaultTheme from 'vitepress/theme'
             View v3 documentation
           </MazLink>
         </div>
-      </template>
+      </template> -->
     </DefaultTheme.Layout>
   </Suspense>
 </template>
 
-<style lang="postcss" scoped>
-.banner {
+<style scoped>
+/* .banner {
   @apply maz-bg-primary-600/90 maz-fixed maz-inset-x-0 maz-top-0 maz-flex maz-flex-wrap maz-flex-center maz-justify-center maz-gap-2 maz-p-2 maz-z-default-backdrop maz-text-white maz-text-center maz-backdrop-blur-lg maz-text-sm;
-}
+} */
 </style>
