b619382988903c654434af0b1b93160e5267b9c4
LouisMazel <me@loicmazuel.com>
2026-01-21 23:59:47 +0100

    feat(@maz-ui/icons): add new iconsAdded icons: MazFacebook, MazInstagram, MazLinkedin MazNpm, MazTwitter and MazQuote

---
+++ packages/icons/src/components/MazLogoTwitter.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite-svg-loader" />
+
+/**
+ * This file is generated automatically, do not manually modify it
+ */
+
+import { defineAsyncComponent, markRaw } from 'vue'
+
+export const MazLogoTwitter = markRaw(defineAsyncComponent(() => import('../../logos/twitter.svg?component')))
