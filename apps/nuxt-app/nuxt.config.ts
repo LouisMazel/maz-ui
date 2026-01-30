eb0f79b9415d74ce0d85351c0f6e777652cb3d1f
LouisMazel <me@loicmazuel.com>
2026-01-30 15:27:06 +0100

    feat(nuxt-app): add font imports

--- apps/nuxt-app/nuxt.config.ts
+++ apps/nuxt-app/nuxt.config.ts
@@ -10,6 +10,14 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'Dev App - Maz-UI',
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap',
+        },
+      ],
     },
   },
   css: ['~/css/main.css'],
