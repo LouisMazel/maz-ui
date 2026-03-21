54db7d48e247d06fa4204d78113aab67e8c92be6
LouisMazel <me@loicmazuel.com>
2026-03-21 12:02:01 +0100

    chore(vue-app): add components view

--- apps/vue-app/src/router/index.ts
+++ apps/vue-app/src/router/index.ts
@@ -1,22 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+export const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/HomeView.vue'),
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
+  },
+  {
+    path: '/color',
+    name: 'color',
+    component: () => import('../views/ColorView.vue'),
+  },
+  {
+    path: '/component',
+    name: 'component',
+    component: () => import('../views/ComponentView.vue'),
+  },
+]
 export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/color',
-      name: 'color',
-      component: () => import('../views/ColorView.vue'),
-    },
-  ],
+  routes,
 })
