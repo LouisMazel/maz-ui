c4afd6d3132d00adb2e87865dfc98cf6c423743c
LouisMazel <me@loicmazuel.com>
2026-05-05 16:45:46 +0200

    test(@maz-ui/nuxt): bump branches coverage threshold to 100

--- packages/nuxt/vitest.config.ts
+++ packages/nuxt/vitest.config.ts
@@ -75,7 +75,7 @@ export default defineConfig({
       thresholds: {
         lines: 100,
         functions: 100,
-        branches: 99.25,
+        branches: 100,
         statements: 100,
         autoUpdate: !process.env.CI,
       },
